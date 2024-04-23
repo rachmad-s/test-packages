4967dec00a800645a4af98eda21269d6fc1aafe7
rsyaefullah <rsyaefullah@pvg.co.id>
2024-04-23 15:20:25 +0700

    build package

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,8 @@
 export declare const helloWorld: () => string;
+/**
+ * adding two number
+ * @param a number 1
+ * @param b number 2
+ * @returns number - result
+ */
+export declare const testFunction: (a: number, b: number) => number;
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = function () {
+exports.testFunction = exports.helloWorld = void 0;
+var helloWorld = function () {
     return 'Howdy!';
 };
+exports.helloWorld = helloWorld;
+/**
+ * adding two number
+ * @param a number 1
+ * @param b number 2
+ * @returns number - result
+ */
+var testFunction = function (a, b) {
+    return a + b;
+};
+exports.testFunction = testFunction;
 //# sourceMappingURL=index.js.map
