010a36241fa14162c57ac0b4325d55732de7725b
rsyaefullah <rsyaefullah@pvg.co.id>
2024-04-23 15:17:24 +0700

    testing publish package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export const helloWorld = () => {
     return 'Howdy!'
 }
+
+/**
+ * adding two number
+ * @param a number 1
+ * @param b number 2
+ * @returns number - result
+ */
+export const testFunction = (a: number, b: number): number => {
+    return a + b
+} 
